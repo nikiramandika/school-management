d8bea1535fba42f72a684e570365dd215938d0d3
finorosatabitha <finorosatabitha2118@gmail.com>
2025-05-14 22:51:24 +0700

    Update halaman page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { GrAnnounce } from "react-icons/gr";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4">
-      <div className="hidden md:flex items-center gap-2 tex-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="logo" width={14} height={14} />
         <input
           type="text"
